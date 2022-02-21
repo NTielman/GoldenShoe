96ab0daba70ee4c62cb8f002e0c134afac324e40
Nneka Tielman <khalie_nne@yahoo.com>
2022-02-21 03:12:44 +0100

    added error handling to sizes, colours and images functions

--- user_queries.py
+++ user_queries.py
@@ -39,7 +39,6 @@ def create_review(buyer_id, product_id, rating, description, image1, image2, ima
             buyer=User.get_by_id(buyer_id), product=Product.get_by_id(product_id), rating=rating, text_body=description, review_image1=image1, review_image2=image2, review_image3=image3
         )
     except peewee.PeeweeException:
-        flash("Could not add your review, please try again later", 'danger')
         return False
 
 
@@ -68,6 +67,17 @@ def get_product_base(product_id):
     except peewee.DoesNotExist:
         return False
 
+
+def get_default_product(base_product_id):
+    '''returns first product variant in stock'''
+    try:
+        base_product = Product.get_by_id(base_product_id)
+        default_variant = ProductUnit.get(ProductUnit.prod_parent == base_product, ProductUnit.qty > 0)
+        return model_to_dict(default_variant, backrefs=True)
+    except peewee.DoesNotExist:
+        return False
+
+
 def get_product_unit(product_id):
     '''finds and returns a specific product variant from database'''
     try:
@@ -79,40 +89,50 @@ def get_product_unit(product_id):
 
 def get_default_product_images(product_id):
     '''returns a list of product_images if any'''
-    default_product = ProductUnit.get_by_id(product_id)
-    images = [image.image_url for image in default_product.images]
-    return images
+    try:
+        default_product = ProductUnit.get_by_id(product_id)
+        images = [image.image_url for image in default_product.images]
+        return images
+    except peewee.DoesNotExist:
+        return False
 
 
 def get_product_sizes(product_id):
     '''returns a list of product sizes if any'''
-    product = Product.get_by_id(product_id)
-    product_variants = product.variants
-    sizes = list(set(prod.size for prod in product_variants))
-    return sizes
+    try:
+        product = Product.get_by_id(product_id)
+        product_variants = product.variants
+        sizes = list(set(prod.size.size_value for prod in product_variants))
+        return sizes
+    except peewee.PeeweeException:
+        return False
 
 
 def get_product_colours(product_id):
     '''returns a list of product colours if any'''
-    product = Product.get_by_id(product_id)
-    product_variants = product.variants
-    colours = list(set(prod.colour for prod in product_variants))
-    return colours
+    try:
+        product = Product.get_by_id(product_id)
+        product_variants = product.variants
+        colours = list(set(prod.colour.colour_value for prod in product_variants))
+        return colours
+    except peewee.PeeweeException:
+        return False
 
 
 def get_product_reviews(product_id):
     '''returns a list of product reviews if any'''
-    product = Product.get_by_id(product_id)
-    reviews = [model_to_dict(review) for review in product.reviews]
-    return reviews
+    try:
+        product = Product.get_by_id(product_id)
+        reviews = [model_to_dict(review) for review in product.reviews]
+        return reviews
+    except peewee.PeeweeException:
+        return False
 
-# TODO change to produnit?
-def get_newest_products():
-    '''returns 7 products in database sorted by date added'''
+
+def get_products():
+    '''returns list of products in database'''
     query = (Product
-             .select()
-             .order_by(Product.date_added.desc())
-             .limit(7))
+             .select())
     products = [model_to_dict(product) for product in query]
     return products
 
